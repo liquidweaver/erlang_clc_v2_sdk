a14be28966a6412cde994c83d8b3ea4db8daf474
Brian Haggard <brihagg@gmail.com>
2015-06-27 01:21:31 -0500

    First failing UAT

---
+++ integration/datacenter_test.erl
@@ -0,0 +1,8 @@
+-module( integration_test ).
+-include( "test_fixture.hrl" ).
+
+setup() ->
+  application:start( clc_v2 ).
+
+clc_v2_datacenters_returns_a_list_with_ca1_in_first_element() ->
+  ?assertMatch( [ #{ <<"id">> := <<"ca1">> }  | _Ignore ], clc_v2:datacenters() ).
