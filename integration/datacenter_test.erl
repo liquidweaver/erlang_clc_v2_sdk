2e0086a6418f4e80028d56fa434b16dd80b5afb6
Joshua Weaver <joshuaweaver@gmail.com>
2015-06-27 15:19:07 -0500

    Correct integration test

--- integration/datacenter_test.erl
+++ integration/datacenter_test.erl
@@ -5,4 +5,5 @@ setup() ->
   application:start( clc_v2 ).
 
 clc_v2_datacenters_returns_a_list_with_ca1_in_first_element() ->
-  ?assertMatch( [ #{ <<"id">> := <<"ca1">> }  | _Ignore ], clc_v2:datacenters() ).
+  {ok, AuthRef } = clc_v2:login( <<"username">>, <<"password">> ),
+  ?assertMatch( [ #{ <<"id">> := <<"ca1">> }  | _Ignore ], clc_v2:datacenters(AuthRef) ).
