b099316b5d7f36fd8ff0307798b92ee81f9f8f41
Joshua Weaver <joshuaweaver@gmail.com>
2015-06-27 15:07:24 -0500

    login api call

--- src/clc_v2.erl
+++ src/clc_v2.erl
@@ -1,5 +1,12 @@
 -module( clc_v2 ).
--export( [datacenters/0] ).
+-export( [
+  login/2,
+  datacenters/0
+  ] ).
+
+-spec login( Username::binary(), Password::binary ) -> clc_v2_auth:auth_ref().
+login( Username, Password ) ->
+  clc_v2_auth_sup:create_worker( Username, Password ).
 
 datacenters() ->
   clc_v2_datacenters:get().
--- test/clc_v2_tests.erl
+++ test/clc_v2_tests.erl
@@ -8,3 +8,11 @@ clc_v2_datacenters_delgates_to_datacenters_get() ->
   ?assertEqual(datacenters1, clc_v2:datacenters()),
 
   ?called(clc_v2_datacenters, get, []).
+
+login_creates_new_auth_worker_under_auth_supervisor() ->
+  ?meck( clc_v2_auth_sup, [non_strict] ),
+  ?stub( clc_v2_auth_sup, create_worker, 2, {ok, authref1}),
+
+  ?assertEqual( {ok, authref1 }, clc_v2:login( username1, password1 ) ),
+
+  ?called( clc_v2_auth_sup, create_worker, [username1, password1] ).
