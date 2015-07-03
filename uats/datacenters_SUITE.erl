060b0516210fd599ebec6b8adac56e94e4b1ef53
Brian Haggard <brihagg@gmail.com>
2015-07-02 19:35:31 -0500

    Start the application in the UAT for now

--- uats/datacenters_SUITE.erl
+++ uats/datacenters_SUITE.erl
@@ -6,9 +6,8 @@
 all() -> [clc_v2_datacenters_returns_a_list_with_ca1_in_first_element].
 
 clc_v2_datacenters_returns_a_list_with_ca1_in_first_element(_Config) ->
+  {ok, _Pid} = application:ensure_all_started( clc_v2 ),
   AuthRef = clc_v2:login( <<"username">>, <<"password">> ),
-  [ #{ <<"id">> := <<"ca1">> }  | _Tail ] = clc_v2:datacenters(AuthRef).
+  [ #{ <<"id">> := <<"ca1">> }  | _Tail ] = clc_v2:datacenters(AuthRef),
+  application:stop( clc_v2 ).
 
-
-%setup() ->
-%  application:start( clc_v2 ).
