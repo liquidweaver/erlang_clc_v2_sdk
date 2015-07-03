d156fc4c5ca27f1ec87f573edd809c54c94ee83b
Brian Haggard <brihagg@gmail.com>
2015-07-03 15:43:45 -0500

    Reuse auth ref in test suite

--- uats/datacenters_SUITE.erl
+++ uats/datacenters_SUITE.erl
@@ -12,21 +12,22 @@ suite() ->
 
 init_per_suite(Config) ->
   {ok, _Pid} = application:ensure_all_started( clc_v2 ),
-  Config.
+  AuthRef = clc_v2:login( <<"username">>, <<"password">> ),
+  [{ auth_ref, AuthRef } | Config ].
 
 end_per_suite(_Config) ->
   application:stop( clc_v2 ).
 
-clc_v2_datacenters_returns_a_list_with_ca1_in_first_element(_Config) ->
-  AuthRef = clc_v2:login( <<"username">>, <<"password">> ),
+clc_v2_datacenters_returns_a_list_with_ca1_in_first_element(Config) ->
+  AuthRef = proplists:get_value( auth_ref, Config ),
   Datacenters = clc_v2:datacenters(AuthRef),
   io:format( "Datacenters: ~p~n", [Datacenters] ),
   [ #{ <<"id">> := <<"ca1">> }  | _Tail ] = Datacenters,
   true = (length(Datacenters) > 1),
   ok.
 
-clc_v2_datacenter_returns_a_single_datacenter(_Config) ->
-  AuthRef = clc_v2:login( <<"username">>, <<"password">> ),
+clc_v2_datacenter_returns_a_single_datacenter(Config) ->
+  AuthRef = proplists:get_value( auth_ref, Config ),
   Datacenter = clc_v2:datacenter(AuthRef, <<"ca1">>),
   io:format( "Datacenter: ~p~n", [Datacenter] ),
   #{ <<"id">> := <<"ca1">> } = Datacenter,
