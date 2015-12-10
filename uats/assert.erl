02206bc2efb546a0f7c8ce7e219ed7db507357fb
Dan Schwabe <dan.schwabe@ctl.io>
2015-12-09 22:37:48 -0600

    reduce log noise

--- uats/assert.erl
+++ uats/assert.erl
@@ -1,22 +1,8 @@
 -module(assert).
 -export([equal/2]).
 
-equal(Expected, Actual) when is_map(Expected) ->
-  Keys = maps:keys(Expected),
-  ValuesEqual = fun(Key) ->
-                    Expected1 = maps:get(Key,Expected),
-                    Actual1 = maps:get(Key,Actual),
-                    equal(Expected1,Actual1)
-                end,
-  lists:all(ValuesEqual, Keys);
-equal([Expected | ExpectedTail], [Actual | ActualTail])
-                                  when is_integer(Expected) =:= false ->
-  equal(Expected, Actual),
-  equal(ExpectedTail, ActualTail);
-equal([],[]) ->
-  true;
 equal(Expected, Actual) ->
-  ct:pal("  expected: ~p~n    actual: ~p~n", [Expected,Actual]),
-  Expected =:= Actual.
+  ct:pal("  expected: ~p~n    actual: ~p~n", [Expected, Actual]),
+  Expected = Actual.
 
 
